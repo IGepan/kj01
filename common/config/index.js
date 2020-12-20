6914aacc77ee72d9254d74f195206369cf75aa15
caijian <@gmail.com>
2020-12-20 22:58:38 +0800

    commit

--- common/config/index.js
+++ common/config/index.js
@@ -4,17 +4,17 @@ define(function () {
   var configs = [
     {
 
-      //正式环境
-      // authUrl: 'http://fapi.kj01.cn/tsOauth2',  // ts zuul
-      // baseUrl: 'http://fapi.kj01.cn/tsAdmin',// ts zuul
-      // assisUrl: 'http://fapi.kj01.cn/tsAssistant', // ts zuul
-      // statisticsUrl: 'http://sts.kj01.cn', // 统计地址
-      // imgUploadUrl: 'http://fileup.kj01.cn', // 文件上传服务
-      // imgVerify: 'http://ver.kj01.cn/', // 滑块验证码
-      // cmsUrl: 'https://cynews.liyantech.cn',
-      // companyApi: 'https://www.dsjscpx.com',
-      // bigData: 'http://47.105.63.195:801/',
-      // datacq: 'https://www.datacq.com.cn/'
+      正式环境
+      authUrl: 'http://fapi.kj01.cn/tsOauth2',  // ts zuul
+      baseUrl: 'http://fapi.kj01.cn/tsAdmin',// ts zuul
+      assisUrl: 'http://fapi.kj01.cn/tsAssistant', // ts zuul
+      statisticsUrl: 'http://sts.kj01.cn', // 统计地址
+      imgUploadUrl: 'http://fileup.kj01.cn', // 文件上传服务
+      imgVerify: 'http://ver.kj01.cn/', // 滑块验证码
+      cmsUrl: 'https://cynews.liyantech.cn',
+      companyApi: 'https://www.dsjscpx.com',
+      bigData: 'http://47.105.63.195:801/',
+      datacq: 'https://www.datacq.com.cn/'
 
       //本地环境
       // authUrl: 'http://127.0.0.1:9769/tsOauth2',
@@ -29,17 +29,17 @@ define(function () {
       // datacq: 'https://www.datacq.com.cn/'
 
 
-      //测试环境
-      authUrl: 'http://222.180.163.242:9769/tsOauth2',
-      baseUrl: 'http://222.180.163.242:9769/tsAdmin',
-      assisUrl: 'http://222.180.163.242:9769/tsAssistant',
-      statisticsUrl: 'http://222.180.163.242:9764', // 统计地址 配置域名
-      imgUploadUrl: 'http://222.180.163.230:8710', // 文件上传服务 配置域名
-      imgVerify: 'http://222.180.163.242:8720/', // 滑块验证码 配置域名
-      // cmsUrl: 'https://cynews.liyantech.cn',
-      companyApi: 'http://47.108.80.38:8304',
-      bigData: 'http://47.105.63.195:801/',
-      datacq: 'https://www.datacq.com.cn/'
+      // //测试环境
+      // authUrl: 'http://222.180.163.242:9769/tsOauth2',
+      // baseUrl: 'http://222.180.163.242:9769/tsAdmin',
+      // assisUrl: 'http://222.180.163.242:9769/tsAssistant',
+      // statisticsUrl: 'http://222.180.163.242:9764', // 统计地址 配置域名
+      // imgUploadUrl: 'http://222.180.163.230:8710', // 文件上传服务 配置域名
+      // imgVerify: 'http://222.180.163.242:8720/', // 滑块验证码 配置域名
+      // // cmsUrl: 'https://cynews.liyantech.cn',
+      // companyApi: 'http://47.108.80.38:8304',
+      // bigData: 'http://47.105.63.195:801/',
+      // datacq: 'https://www.datacq.com.cn/'
     }
 
   ];
