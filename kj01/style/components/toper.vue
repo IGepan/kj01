749c757adc17b45eb3f0b9d9a5e8efff3c245695
huangdaxia <710368779@qq.com>
2020-12-29 11:31:54 +0800

    head调整

--- kj01/style/components/toper.vue
+++ kj01/style/components/toper.vue
@@ -147,6 +147,17 @@
               </div>
           </div>
           <div
+              class="c-hover-item"
+              onclick="window.open('http://www5c1.53kf.com/webCompany.php?arg=10113491&style=1&language=zh-cn&lytype=0&charset=utf-8&kflist=off&zdkf_type=1&referer=http%3A%2F%2Fwww.53kf.com%2Findex.php&keyword=http%3A//www.53kf.com&timeStamp=1604995029140&ucust_id=')"
+          >
+            <div class="c-hover-icon">
+              <span class="icon-fuwu iconfont"></span>
+            </div>
+            <div class="c-hover-txt">
+              <span>客服</span>
+            </div>
+          </div>
+          <div
                   class="c-hover-item"
                   @click="action('top')"
           >
