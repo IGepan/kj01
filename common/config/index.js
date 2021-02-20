ee23b163b30b0b43fd0991378527532f0c6b88ec
唐海燕 <tanghaiyan@qw1000.cn>
2021-02-20 14:29:55 +0800

    修改环境配置

--- common/config/index.js
+++ common/config/index.js
@@ -30,28 +30,28 @@ define(function () {
 
       //
       // //测试环境
-      // authUrl: 'http://222.180.163.242:9769/tsOauth2',
-      // baseUrl: 'http://222.180.163.242:9769/tsAdmin',
-      // assisUrl: 'http://222.180.163.242:9769/tsAssistant',
-      // statisticsUrl: 'http://testweb.kj01.cn/as', // 统计地址 配置域名
-      // imgUploadUrl: 'http://222.180.163.230:8710', // 文件上传服务 配置域名
-      // imgVerify: 'http://222.180.163.242:8720/', // 滑块验证码 配置域名
-      // // cmsUrl: 'https://cynews.liyantech.cn',
-      // companyApi: 'https://www.dsjscpx.com',
-      // bigData: 'http://47.105.63.195:801/',
-      // datacq: 'https://www.datacq.com.cn/'
-
-        // 升级测试环境
-      authUrl: 'http://172.28.143.165:9769/tsOauth2',
-      baseUrl: 'http://172.28.143.165:9769/tsAdmin',
-      assisUrl: 'http://172.28.143.165:9769/tsAssistant',
-      statisticsUrl: 'http://172.28.143.165:9764', // 统计地址 配置域名
-      imgUploadUrl: 'http://172.28.143.165:8710', // 文件上传服务 配置域名
-      imgVerify: 'http://172.28.143.165:8720/', // 滑块验证码 配置域名
+      authUrl: 'http://222.180.163.242:9769/tsOauth2',
+      baseUrl: 'http://222.180.163.242:9769/tsAdmin',
+      assisUrl: 'http://222.180.163.242:9769/tsAssistant',
+      statisticsUrl: 'http://testweb.kj01.cn/as', // 统计地址 配置域名
+      imgUploadUrl: 'http://222.180.163.230:8710', // 文件上传服务 配置域名
+      imgVerify: 'http://222.180.163.242:8720/', // 滑块验证码 配置域名
       // cmsUrl: 'https://cynews.liyantech.cn',
       companyApi: 'https://www.dsjscpx.com',
       bigData: 'http://47.105.63.195:801/',
       datacq: 'https://www.datacq.com.cn/'
+
+        // 升级测试环境
+      // authUrl: 'http://172.28.143.165:9769/tsOauth2',
+      // baseUrl: 'http://172.28.143.165:9769/tsAdmin',
+      // assisUrl: 'http://172.28.143.165:9769/tsAssistant',
+      // statisticsUrl: 'http://172.28.143.165:9764', // 统计地址 配置域名
+      // imgUploadUrl: 'http://172.28.143.165:8710', // 文件上传服务 配置域名
+      // imgVerify: 'http://172.28.143.165:8720/', // 滑块验证码 配置域名
+      // // cmsUrl: 'https://cynews.liyantech.cn',
+      // companyApi: 'https://www.dsjscpx.com',
+      // bigData: 'http://47.105.63.195:801/',
+      // datacq: 'https://www.datacq.com.cn/'
     }
 
   ];
