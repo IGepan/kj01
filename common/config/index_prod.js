1641a3ac1f9828a1da30fba6d38fb51a345c2d52
wql <2837280443@qq.com>
2021-03-05 09:28:42 +0800

    配置https->http

--- common/config/index_prod.js
+++ common/config/index_prod.js
@@ -15,12 +15,12 @@ define(function () {
       resourceUrl:'http://city-service.cykj01.com/',//资源中心的数据接口
     },
     {
-      authUrl: 'https://ts-zuul.liyantech.cn/tsOauth2',
-      baseUrl: 'https://ts-zuul.liyantech.cn/tsAdmin',
-      assisUrl: 'https://ts-zuul.liyantech.cn/tsAssistant',
-      statisticsUrl: 'https://as.liyantech.cn', // 统计地址
-      imgUploadUrl: 'https://fs.liyantech.cn', // 文件上传服务
-      imgVerify: 'https://captcha.liyantech.cn/', // 滑块验证码
+      authUrl: 'http://ts-zuul.liyantech.cn/tsOauth2',
+      baseUrl: 'http://ts-zuul.liyantech.cn/tsAdmin',
+      assisUrl: 'http://ts-zuul.liyantech.cn/tsAssistant',
+      statisticsUrl: 'http://as.liyantech.cn', // 统计地址
+      imgUploadUrl: 'http://fs.liyantech.cn', // 文件上传服务
+      imgVerify: 'http://captcha.liyantech.cn/', // 滑块验证码
       cmsUrl: 'https://kj01news.liyantech.cn',
       companyApi: 'http://47.108.80.38:8304',
       bigData: 'http://47.105.63.195:801/',
