1acdc92fbcd4c8f46bf925916478cd2c385165c3
jack <710368779@qq.com>
2021-03-23 10:31:42 +0800

    test

--- common/config/index.js
+++ common/config/index.js
@@ -1,5 +1,5 @@
 define(function () {
-  var isConfigIndex = ['main.liyantech.cn:8081', 'kj01.liyantech.cn:8080', 'intell.liyantech.cn:8083','kjzx.liyantech.cn:8082'].indexOf(location.host);
+  var isConfigIndex = ['main.liyantech.cn:8081', 'kj01.liyantech.cn:8080', 'intell.liyantech.cn:8083','kjzx.liyantech.cn:8082','testweb.kj01.cn'].indexOf(location.host);
   var configs = [
     {
       // authUrl: 'http://ts-zuul.liyantech.cn/tsOauth2',
@@ -56,6 +56,17 @@ define(function () {
       imgUploadUrl: 'http://fs.liyantech.cn', // 文件上传服务
       statisticsUrl: 'http://as.liyantech.cn', // 统计地址
       reportDocumentUrl: 'http://39.100.48.36:3093', // 分析报告-论文，新闻
+    },{
+      authUrl: 'http://222.180.163.242:9769/tsOauth2',
+      baseUrl: 'http://222.180.163.242:9769/tsAdmin',
+      assisUrl: 'http://222.180.163.242:9769/tsAssistant',
+      statisticsUrl: 'http://testweb.kj01.cn/as', // 统计地址 配置域名
+      imgUploadUrl: 'http://222.180.163.230:8710', // 文件上传服务 配置域名
+      imgVerify: 'http://222.180.163.242:8720/', // 滑块验证码 配置域名
+      // cmsUrl: 'https://cynews.liyantech.cn',
+      companyApi: 'https://www.dsjscpx.com',
+      bigData: 'http://47.105.63.195:801/',
+      datacq: 'https://www.datacq.com.cn/'
     }
   ];
   isConfigIndex === -1 && (isConfigIndex = 0);
