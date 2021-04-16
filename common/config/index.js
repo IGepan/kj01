0e4acfead34e85c42863ac776e1b6e32563348d3
jack <710368779@qq.com>
2021-04-16 14:25:55 +0800

    修改

--- common/config/index.js
+++ common/config/index.js
@@ -5,16 +5,16 @@ define(function () {
     {
 
       //正式环境
-      authUrl: 'https://ts-zuul.kj01.cn/tsOauth2',  // ts zuul
-      baseUrl: 'https://ts-zuul.kj01.cn/tsAdmin',// ts zuul
-      assisUrl: 'https://ts-zuul.kj01.cn/tsAssistant', // ts zuul
-      statisticsUrl: 'https://www.kj01.cn/asapi', // 统计地址
-      imgUploadUrl: 'https://fs.kj01.cn', // 文件上传服务
-      imgVerify: 'https://captcha.kj01.cn/', // 滑块验证码
-      cmsUrl: 'https://cms.kj01.cn/',
-      companyApi: 'https://www.dsjscpx.com',
-      bigData: 'https://47.105.63.195:801/',
-      datacq: 'https://www.datacq.com.cn/'
+      // authUrl: 'https://ts-zuul.kj01.cn/tsOauth2',  // ts zuul
+      // baseUrl: 'https://ts-zuul.kj01.cn/tsAdmin',// ts zuul
+      // assisUrl: 'https://ts-zuul.kj01.cn/tsAssistant', // ts zuul
+      // statisticsUrl: 'https://www.kj01.cn/asapi', // 统计地址
+      // imgUploadUrl: 'https://fs.kj01.cn', // 文件上传服务
+      // imgVerify: 'https://captcha.kj01.cn/', // 滑块验证码
+      // cmsUrl: 'https://cms.kj01.cn/',
+      // companyApi: 'https://www.dsjscpx.com',
+      // bigData: 'https://47.105.63.195:801/',
+      // datacq: 'https://www.datacq.com.cn/'
 
       // 本地环境
       // authUrl: 'http://127.0.0.1:9769/tsOauth2',
