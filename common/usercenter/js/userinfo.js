4835b34138e66b91787b4a363e802a74b5c63923
jack <710368779@qq.com>
2021-04-25 16:36:47 +0800

    普通用户类型中增加了单位名称和职务字段（测试）

--- common/usercenter/js/userinfo.js
+++ common/usercenter/js/userinfo.js
@@ -10,8 +10,8 @@ require(['/common/js/require.config.js'], function () {
         httpUser: httpUser,
         httpCom: httpCom,
         formData: {
-          companyName: '', //单位名称
-          job: '', //职位
+          // companyName: '', //单位名称
+          // job: '', //职位
           headImg: '',
           userBasicId: '', // 用户基本信息主键
           userName: '', // 用户名
@@ -274,6 +274,7 @@ require(['/common/js/require.config.js'], function () {
               formData.code = data.code;
               console.log(formData, formData.code)
               httpUser[vm.alias.submitFun[data.identityType]](formData).then(function (resp) {
+
                 if (resp.code == 'rest.success') {
                   vm.$dialog.showToast('保存成功');
                   vm.$httpCom.webCommonUser().then(function (res) {
