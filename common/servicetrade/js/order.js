1a5ddcdd8b1016f4b30623778ffbe888f31cc5d5
jack <710368779@qq.com>
2021-05-11 19:12:52 +0800

    易智商城立即购买修改

--- common/servicetrade/js/order.js
+++ common/servicetrade/js/order.js
@@ -396,7 +396,12 @@ require([baseUrlPath + '/common/js/require.config.js'], function () {
           }).then(function (res) {
             if (res.code == 'rest.success') {
               detail.number = number
-              detail.subtotal = detail.price !== undefined ? (detail.number * detail.price) : (detail.number * detail.minPrice)
+              //如果价格为面议或者自定义，则根据协议单价计算金额
+              if (detail.choosePriceTag!=='0'||detail.choosePriceTag!=='1'){
+                detail.subtotal = detail.price !== undefined ? (detail.number * detail.price) : (detail.number * detail.minPrice)
+              }else{
+                detail.subtotal = detail.protocolPrice * detail.number
+              }
             }
           })
         },
