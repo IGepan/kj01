e52b72e3ecc34a782b9dd4dc4f24dd51b8fea9c5
jack <710368779@qq.com>
2021-07-27 17:03:06 +0800

    修改证书下载问题

--- common/usercenter/js/userMarketAuthForm.js
+++ common/usercenter/js/userMarketAuthForm.js
@@ -258,29 +258,31 @@ require(['/common/js/require.config.js'], function () {
                         var lastIndex = path.lastIndexOf("/")
                         var imgName = path.substring(lastIndex)
                         userCenterApi.getZmImg({imgUrl: imgUrl}).then(res => {
-                           saveAs(res,"证书"+imgName)
+                            console.log(res)
+                           saveAs(res,"证书"+imgName,{ type: 'image/png;charset=utf-8' })
                         });
+                       // saveAs(imgUrl,"证书"+imgName,{ type: 'image/png;charset=utf-8' })
 
                     },
-                    download(link,picName) {
-                        let img = new Image()
-                        img.setAttribute('crossOrigin', 'Anonymous')
-                        img.crossOrigin
-                        img.onload = function(){
-                            let canvas = document.createElement('canvas')
-                            let context = canvas.getContext('2d')
-                            canvas.width = img.width
-                            canvas.height = img.height
-                            context.drawImage(img, 0, 0, img.width, img.height)
-                            let url = canvas.toDataURL('images/png')
-                            let a = document.createElement('a')
-                            let event = new MouseEvent('click')
-                            a.download = picName || 'default.png'
-                            a.href = url
-                            a.dispatchEvent(event)
-                        }
-                        img.src = link
-                    },
+                    // download(link,picName) {
+                    //     let img = new Image()
+                    //     img.setAttribute('crossOrigin', 'Anonymous')
+                    //     img.crossOrigin
+                    //     img.onload = function(){
+                    //         let canvas = document.createElement('canvas')
+                    //         let context = canvas.getContext('2d')
+                    //         canvas.width = img.width
+                    //         canvas.height = img.height
+                    //         context.drawImage(img, 0, 0, img.width, img.height)
+                    //         let url = canvas.toDataURL('images/png')
+                    //         let a = document.createElement('a')
+                    //         let event = new MouseEvent('click')
+                    //         a.download = picName || 'default.png'
+                    //         a.href = url
+                    //         a.dispatchEvent(event)
+                    //     }
+                    //     img.src = link
+                    // },
 
                     // 查询用户信息
                     myCertificagetUserInfo: function () {
