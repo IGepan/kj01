c4dd0c4256c0b99a2daa7290ae642ff090dbc224
jack <710368779@qq.com>
2021-08-17 15:58:01 +0800

    修改品牌活动

--- kj01/style/js/alist.js
+++ kj01/style/js/alist.js
@@ -143,7 +143,7 @@ require(['/common/js/require.config.js'], function () {
               type && (this.navIndex = this.options.navIndexOpts[type]);
               this.getDicList(this.dicOptsSet);
               if(this.searchForm.activeType == '390092837996355585'){
-                this.searchForm.activeIndex === '5'
+                this.searchForm.activeIndex === '4'
               }
               this.getDataList(function(){
                 // 首次进入页面，展开二级导航
@@ -170,13 +170,13 @@ require(['/common/js/require.config.js'], function () {
             },
             getDataList: function (call) {
               var vm = this;
-              if (this.searchForm.activeTypeDisplay === '品牌活动') {
+              if (this.searchForm.activeTypeDisplay === '4') {
                 this.isActive = true//隐藏默认排序
                 //品牌活动
                 var searchForm = this.searchForm
                 // 检查二级是否点击 topicCustomTag
                 try {
-                  for(let item of vm.options.searchOpts[0].dictIInfos[5].children){
+                  for(let item of vm.options.searchOpts[0].dictIInfos[4].children){
                     item.selected ? searchForm.topicCustomTag = item.value:''
                   }
                 }catch (e){}
