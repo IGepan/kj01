1f7eb74212b53d9891989e596c751b0d164c6815
Administrator <admin@example.com>
2021-08-24 10:24:40 +0800

    1

--- common/config/index.js
+++ common/config/index.js
@@ -25,7 +25,7 @@ define(function () {
     {
       // 本地环境-yzw
       authUrl: 'http://192.168.0.200:9769/tsOauth2',
-      baseUrl: 'http://192.168.0.200:9769/tsAdmin',
+      baseUrl: 'http://127.0.0.1:9769/tsAdmin',
       // baseMarketUrl: 'http://192.168.0.30:8901',
       // baseMarketUrl: 'http://192.168.0.10:8901',
       baseMarketUrl: 'http://192.168.0.200:9769/z-ts',//8901 ts 8900 tm ||
@@ -44,7 +44,7 @@ define(function () {
     {
       //测试环境
       authUrl: 'http://222.180.163.242:9769/tsOauth2',
-      baseUrl: 'http://127.0.0.1:9769/tsAdmin',
+      baseUrl: 'http://192.168.0.200:9769/tsAdmin',
       baseMarketUrl: 'http://222.180.163.242:9769/z-ts',//8901 ts 8900 tm ||
       baseSchoolUrl: 'http://222.180.163.230:8905',//学堂
       baseSchoolOutUrl: 'http://222.180.163.238:5555',//学堂跳转外网
