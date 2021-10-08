5e96d27546da175394c6d43fa53fe220c7f2f85a
watchingfun <wenhuan2333@gmail.com>
2021-10-08 11:54:41 +0800

    技术转移 订单业务经纪人填写日志功能

---
+++ common/components/userTechMenu.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in urlData" :key="index">
+        <a :href="item.url" :class="item.active ? 'active' : ''" target="_blank" v-if="item.target">
+          <i :class="item.icon"></i>
+          {{ item.name }}
+        </a>
+        <a v-else :href="item.url" :class="item.active ? 'active' : ''">
+          <i :class="item.icon"></i>
+          {{ item.name }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      urlData: [
+        {
+          url: "/common/usercenter/user_market_auth_form.html",
+          active: false,
+          icon: "el-icon-s-check",
+          name: "资格认证",
+          index: 0,
+        },
+        {
+          url: "/common/usercenter/user_market_tech_achievements.html",
+          active: false,
+          icon: "el-icon-s-finance",
+          name: "技术成果管理",
+          index: 1,
+        },
+
+        {
+          url: "/common/usercenter/user_market_tech_require.html",
+          active: false,
+          icon: "el-icon-s-help",
+          name: "技术需求管理",
+          index: 2,
+        },
+        {
+          url: "/common/usercenter/user_market_tech_transfer.html",
+          active: false,
+          icon: "el-icon-s-platform",
+          name: "技转业务管理",
+          index: 3,
+        },
+        {
+          url: "/common/usercenter/user_market_business_management.html",
+          active: false,
+          icon: "el-icon-s-cooperation",
+          name: "我的业务管理",
+          index: 4,
+        },
+        {
+          url: "/common/usercenter/user_market_business_order.html",
+          active: false,
+          icon: "el-icon-s-order",
+          name: "我的订单",
+          index: 5,
+        },
+        {
+          url: "https://study.kj01.cn/uc/index",
+          active: false,
+          icon: "el-icon-s-custom",
+          name: "我的培训",
+          target: true,
+          index: 6,
+        },
+        {
+          url: "/common/usercenter/user_market_tech_collection.html",
+          active: false,
+          icon: "el-icon-s-claim",
+          name: "收藏",
+          index: 7,
+        },
+      ],
+    };
+  },
+  methods: {},
+  props: ["type"],
+  mounted() {
+    // console.log("this.type", this.type);
+    this.urlData.forEach((element) => {
+      if (this.type == element.index) {
+        element.active = true;
+      }
+    });
+  },
+};
+</script>
+
+<style></style>
