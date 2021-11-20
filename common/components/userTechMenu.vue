6c5a03c96a02a01113fc21fef70f6cce451e62aa
1639016684 <1639016684@qq.com>
2021-11-20 15:09:31 +0800

    技术转移-用户中心

--- common/components/userTechMenu.vue
+++ common/components/userTechMenu.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
     <ul>
-      <li v-for="(item, index) in urlData" :key="index">
+      <li v-for="(item, index) in urlData" :key="index" v-if="isNotSite==false">
         <a :href="item.url" :class="item.active ? 'active' : ''" target="_blank" v-if="item.target">
           <i :class="item.icon"></i>
           {{ item.name }}
         </a>
         <a v-else :href="item.url" :class="item.active ? 'active' : ''">
+          <i :class="item.icon"></i>
+          {{ item.name }}
+        </a>
+      </li>
+      <li v-for="(item, index) in urlData" :key="index" v-else>
+        <a :href="$pathPrefix+item.url" :class="item.active ? 'active' : ''" target="_blank" v-if="item.target">
+          <i :class="item.icon"></i>
+          {{ item.name }}
+        </a>
+        <a v-else :href="$pathPrefix+item.url" :class="item.active ? 'active' : ''">
           <i :class="item.icon"></i>
           {{ item.name }}
         </a>
@@ -19,6 +29,7 @@
 module.exports = {
   data: function () {
     return {
+      isNotSite:false,
       urlData: [
         {
           url: "/common/usercenter/user_market_auth_form.html",
@@ -95,16 +106,25 @@ module.exports = {
       ],
     };
   },
-  methods: {},
+  methods: { //检查是否分站点
+    checkSite: function () {
+      var url = window.location.href
+      var vm = this;
+      if (url.indexOf('/site/') > 0) {
+        vm.isNotSite=true
+    }
+    },
+  },
   props: ["type"],
   mounted() {
+    this.checkSite();
     // console.log("this.type", this.type);
     this.urlData.forEach((element) => {
       if (this.type == element.index) {
         element.active = true;
-
       }
     });
+
     var ID = this.$utils.getReqStr('classId');
       if(ID){
         this.urlData[6].name='培训报名'
