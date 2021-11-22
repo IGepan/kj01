1f1fdd9a2b5a3fbe20a0760ffd03fedc3cb248df
1639016684 <1639016684@qq.com>
2021-11-22 18:16:54 +0800

    开州分站点用户默认头像

--- site/Eg64enV5/style/js/index.js
+++ site/Eg64enV5/style/js/index.js
@@ -485,11 +485,11 @@ require(['/common/js/require.config.js'], function () {
 							if (res.code === "rest.success") {
 								vm.userList=res.result.list;
 								vm.sum1=res.result.total
-								vm.userList.forEach((item)=>{
-									if(!item.headImg){
-										vm.item.headImg.url='./style/images/avatar-boy.gif'
-									}
-								});
+								// vm.userList.forEach((item)=>{
+								// 	if(!item.headImg){
+								// 		vm.item.headImg.url=''
+								// 	}
+								// });
 								vm.$nextTick(()=>{
 									$(".slideFromBox1").slide({
 										titCell: ".hd ul",
